e14af48e28b03f93a60593781d1674a83692f3d8
Michal Trubini <mchl.trubini@gmail.com>
2021-11-02 18:04:02 +0100

    desktop layout finished

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ function toggleMenu() {
         navMenu.style.display = 'none';
     }
 }
+
+const links = document.querySelectorAll('.about__link');
+const highlightOne = document.querySelector('.about__link-border-one');
+const highlightTwo = document.querySelector('.about__link-border-two');
+
+links.forEach(highlightOn);
+
+function highlightOn(link){
+    link.addEventListener('mouseenter',function(){
+        highlightOne.style.opacity = 1;
+        highlightTwo.style.opacity = 1;
+    })
+    link.addEventListener('mouseleave',function(){
+        highlightOne.style.opacity = 0.25;
+        highlightTwo.style.opacity = 0.25;
+    })
+}
